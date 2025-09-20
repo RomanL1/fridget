4ff757cdbfa543f908dd79bf4dffe1dfc572073e
roman <lysserroman@gmail.com>
2025-09-20 14:40:01 +0200

    F - init spring backend

---
+++ src/main/java/ch/fridget/fridget/FridgetApplication.java
@@ -0,0 +1,15 @@
+package ch.fridget.fridget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgetApplication
+{
+
+	public static void main ( String[] args )
+	{
+		SpringApplication.run( FridgetApplication.class, args );
+	}
+
+}
---
+++ src/test/java/ch/fridget/fridget/FridgetApplicationTests.java
@@ -0,0 +1,15 @@
+package ch.fridget.fridget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgetApplicationTests
+{
+
+	@Test
+	void contextLoads ()
+	{
+	}
+
+}
