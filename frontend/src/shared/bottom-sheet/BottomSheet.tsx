8b8b58046388855db5212f638f00c101091e8881
Andri Wandres <andrilucawandres@gmail.com>
2025-10-22 16:25:12 +0200

    refactor(common): expose methods for opening/closing sheet

--- frontend/src/shared/bottom-sheet/BottomSheet.tsx
+++ frontend/src/shared/bottom-sheet/BottomSheet.tsx
@@ -1,19 +1,30 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, RefObject, useImperativeHandle, useState } from 'react';
 
 import styles from './BottomSheet.module.css';
 
+export interface BottomSheetRef {
+  open(): void;
+  close(): void;
+}
+
 export type BottomSheetProps = PropsWithChildren<{
-  isVisible: boolean;
-  onBackdropClick?: () => void;
+  ref: RefObject<BottomSheetRef | null>;
 }>;
 
-export function BottomSheet({ isVisible, children, onBackdropClick }: BottomSheetProps) {
+export function BottomSheet({ ref, children }: BottomSheetProps) {
+  const [isVisible, setVisible] = useState(false);
   const backdropAnimationClass = isVisible ? styles.backdropEntered : styles.backdropExited;
   const bottomSheetAnimationClass = isVisible ? styles.bottomSheetEntered : styles.bottomSheetExited;
 
+  // Expose control methods to the consumer
+  useImperativeHandle(ref, () => ({
+    open: () => setVisible(true),
+    close: () => setVisible(false),
+  }));
+
   return (
     <div className={styles.container}>
-      <div className={`${styles.backdrop} ${backdropAnimationClass}`} onClick={onBackdropClick}>
+      <div className={`${styles.backdrop} ${backdropAnimationClass}`} onClick={() => ref.current?.close()}>
         <div className={`${styles.bottomSheet} ${bottomSheetAnimationClass}`} onClick={(e) => e.stopPropagation()}>
           <div className={styles.bottomSheetContent}>{children}</div>
         </div>
