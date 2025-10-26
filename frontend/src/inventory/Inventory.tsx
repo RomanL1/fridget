d32b36a833238dc8de83656d262d2019aa9c4104
Andri Wandres <andrilucawandres@gmail.com>
2025-10-26 20:41:31 +0100

    provide product card demo

--- frontend/src/inventory/Inventory.tsx
+++ frontend/src/inventory/Inventory.tsx
@@ -1,5 +1,31 @@
-import { BarcodeScannerToggle } from './barcode-scanner-toggle/BarcodeScannerToggle';
+import { custom, fresh, pastExpiration, soonExpiring } from './fixtures/inventory-items';
+import { InventoryItem } from './inventory-item/inventory-item';
+import { InventoryItemCard } from './inventory-item/InventoryItemCard';
 
 export function Inventory() {
-  return <BarcodeScannerToggle />;
+  return (
+    <div style={{ padding: '3rem', display: 'flex', alignContent: 'start', gap: '2rem', flexWrap: 'wrap' }}>
+      <InventoryItems />
+    </div>
+  );
+}
+
+function InventoryItems() {
+  const items = [fresh, soonExpiring, pastExpiration, custom];
+  return items.map((item) => (
+    <InventoryItemCard
+      key={item.inventoryItemId}
+      inventoryItem={item}
+      onEditClick={() => editInventoryItem(item)}
+      onRemoveClick={() => removeInventoryItem(item)}
+    />
+  ));
+}
+
+function editInventoryItem(item: InventoryItem) {
+  console.log(item);
+}
+
+function removeInventoryItem(item: InventoryItem) {
+  console.log(item);
 }
