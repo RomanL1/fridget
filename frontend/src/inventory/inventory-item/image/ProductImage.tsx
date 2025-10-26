a4de2a16fcfe22cd225cfe4b7ce7d97d2406d6c9
Andri Wandres <andrilucawandres@gmail.com>
2025-10-26 20:08:54 +0100

    feat(inventory): add placeholder image

--- frontend/src/inventory/inventory-item/image/ProductImage.tsx
+++ frontend/src/inventory/inventory-item/image/ProductImage.tsx
@@ -1,15 +1,30 @@
 import { Inset } from '@radix-ui/themes';
 
 import styles from './ProductImage.module.css';
+import { CameraIcon } from 'lucide-react';
 
 interface ProductImageProps {
   imageUrl?: string;
 }
 
 export function ProductImage({ imageUrl }: ProductImageProps) {
+  if (!imageUrl) {
+    return <PlaceholderImage />;
+  }
+
   return (
     <Inset clip="padding-box" side="top" pb="current">
       <img src={imageUrl} className={styles.image} />
+    </Inset>
+  );
+}
+
+function PlaceholderImage() {
+  return (
+    <Inset clip="padding-box" side="top" pb="current">
+      <div className={styles.placeholderImage} role="img">
+        <CameraIcon />
+      </div>
     </Inset>
   );
 }
