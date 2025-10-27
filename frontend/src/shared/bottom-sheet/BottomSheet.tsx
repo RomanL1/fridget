244041f3c9232e227a4c379cdab97edc868ea82c
Andri Wandres <35061384+andriwandres@users.noreply.github.com>
2025-10-27 18:54:29 +0100

    F - add bottom sheet component (#38)
    * feat(common): add bottom sheet component
    
    * refactor(common): expose methods for opening/closing sheet
    
    * refactor(common): clear ref when component is un-mounted

---
+++ frontend/src/shared/bottom-sheet/BottomSheet.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren, RefObject, useEffect, useImperativeHandle, useState } from 'react';
+
+import styles from './BottomSheet.module.css';
+
+export interface BottomSheetRef {
+  open(): void;
+  close(): void;
+}
+
+export type BottomSheetProps = PropsWithChildren<{
+  ref: RefObject<BottomSheetRef | null>;
+}>;
+
+export function BottomSheet({ ref, children }: BottomSheetProps) {
+  const [isVisible, setVisible] = useState(false);
+  const backdropAnimationClass = isVisible ? styles.backdropEntered : styles.backdropExited;
+  const bottomSheetAnimationClass = isVisible ? styles.bottomSheetEntered : styles.bottomSheetExited;
+
+  // Expose control methods to the consumer
+  useImperativeHandle(ref, () => ({
+    open: () => setVisible(true),
+    close: () => setVisible(false),
+  }));
+
+  // Clear ref when component is un-mounted
+  useEffect(() => {
+    return () => {
+      ref.current = null;
+    };
+  }, [ref]);
+
+  return (
+    <div className={styles.container}>
+      <div className={`${styles.backdrop} ${backdropAnimationClass}`} onClick={() => ref.current?.close()}>
+        <div className={`${styles.bottomSheet} ${bottomSheetAnimationClass}`} onClick={(e) => e.stopPropagation()}>
+          <div className={styles.bottomSheetContent}>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
