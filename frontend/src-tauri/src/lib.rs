3067eff2163ea27d9da8d49a3bd333fb65a75b4d
Andri Wandres <andrilucawandres@gmail.com>
2025-10-31 15:07:16 +0100

    F - add tauri http plugin

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .setup(|_app| {
             #[cfg(mobile)]
             let _ = _app.handle().plugin(tauri_plugin_barcode_scanner::init());
