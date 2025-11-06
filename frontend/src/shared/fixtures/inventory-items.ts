da74d484907f254ed1d6fecedb163363b228e221
Andri Wandres <andrilucawandres@gmail.com>
2025-11-06 13:30:51 +0100

    fix build errors

--- frontend/src/shared/fixtures/inventory-items.ts
+++ frontend/src/shared/fixtures/inventory-items.ts
@@ -7,8 +7,8 @@ function getTodayOffsetByDays(numberOfDays: number) {
 }
 
 const fresh: InventoryItem = {
-  productId: 1,
-  inventoryItemId: 1,
+  productId: '204047bc-306f-4867-801f-ca45c5a138bf',
+  inventoryItemId: '2b63597c-abc6-440a-9ad4-999e57702a2d',
   productName: 'El Tony Mate',
   brandName: 'El Tony',
   quantity: '330 ml',
@@ -17,16 +17,16 @@ const fresh: InventoryItem = {
 };
 
 const custom: InventoryItem = {
-  productId: 2,
-  inventoryItemId: 2,
+  productId: 'c53f741c-23cc-4a8b-86a7-05c69459ef97',
+  inventoryItemId: 'ee7450fd-d1a5-484a-8352-9b5972b7adb6',
   productName: 'Marmorkuchen (Selbstgebacken)',
   quantity: '2 Stk.',
   bestBeforeDate: getTodayOffsetByDays(7),
 };
 
 const soonExpiring: InventoryItem = {
-  productId: 3,
-  inventoryItemId: 3,
+  productId: 'f006c1e2-1f07-4958-8ffc-3bf69dd7f978',
+  inventoryItemId: 'ba719ac7-2956-4478-89fb-e9d0800ca3f0',
   productName: 'Champignons Bio braun',
   brandName: 'Naturaplan',
   quantity: '250 g',
@@ -35,8 +35,8 @@ const soonExpiring: InventoryItem = {
 };
 
 const pastExpiration: InventoryItem = {
-  productId: 4,
-  inventoryItemId: 4,
+  productId: '7e6cbc90-1731-46c7-a080-8027a40643be',
+  inventoryItemId: 'bf9de342-8c77-4bcc-bb99-984b06ed07fd',
   productName: 'Kochbutter',
   brandName: 'BO Butter GmbH',
   quantity: '250 g',
