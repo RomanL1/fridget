a3a47880586f7cc32db6aa0dca1d6b98692a04b5
Gregor Lazzarato <gregor.lazzarato@stud.hslu.ch>
2025-11-17 19:16:27 +0100

    refactor: changes based on review discussion

--- frontend/src/inventory/inventory-items/InventoryItemGrid.tsx
+++ frontend/src/inventory/inventory-items/InventoryItemGrid.tsx
@@ -18,10 +18,7 @@ export function InventoryItemGrid({ inventoryItems, onEditItem, onRemoveItem }: 
 }
 
 function InventoryItemList({ inventoryItems, onEditItem, onRemoveItem }: InventoryItemGridProps) {
-  // TODO: for testing purposes to generate scrollable content
-  const _inventoryItems = Array(4).fill(inventoryItems).flat();
-
-  return _inventoryItems.map((item) => (
+  return inventoryItems.map((item) => (
     <InventoryItemCard
       key={item.inventoryItemId}
       inventoryItem={item}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,9 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { BarcodeScanner } from './barcode-scanner/BarcodeScanner';
 import { Inventory } from './inventory/Inventory';
 import { authenticate } from './shared/auth';
-
 import './main.css';
-import Root from './Root';
 import { ThemeProvider } from 'next-themes';
+import MainLayout from './MainLayout';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -16,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Theme>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Root />}>
+            <Route path="/" element={<MainLayout />}>
               <Route index element={<Inventory />} />
               <Route path="recipes" />
               <Route path="grocery-list" />
